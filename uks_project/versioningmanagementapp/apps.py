48c4128471ea6f1fc0356ea36cb566f693d67df0
jovicpetra <jovic.petra99@gmail.com>
2023-05-23 18:45:56 +0200

    feat: django apps structure created

--- uks_project/versioningmanagementapp/apps.py
+++ uks_project/versioningmanagementapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BackendAppConfig(AppConfig):
+class VersioningmanagementappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend_app'
+    name = 'versioningmanagementapp'
