cba920543734e75daa01fe803fae9a3c4d416ec1
jovicpetra <jovic.petra99@gmail.com>
2023-05-24 20:14:23 +0200

    feat: initial model added

--- uks_project/users_management_app/models.py
+++ uks_project/users_management_app/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 
 # File for: User, Account
+
+class User(models.Model):
+    username = models.CharField(max_length=100, null=False, unique=True)
+    email = models.CharField(max_length=100, null=False, unique=True)
+    given_name = models.CharField(max_length=100, null=False)
+    family_name = models.CharField(max_length=100, null=False)
+    bio = models.CharField(max_length=100, null=True) #optional
+    url = models.CharField(max_length=100, null=True) #optional
+
