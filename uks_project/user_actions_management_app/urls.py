5cafd549e171e431dd2c6ebd0ff7dd4ee9d6bafd
jovicpetra <jovic.petra99@gmail.com>
2023-05-28 16:57:43 +0200

    feat: apis for comments handling fixed

--- uks_project/user_actions_management_app/urls.py
+++ uks_project/user_actions_management_app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('comments/', views.get_all_comments, name='all_comments'),
     path('pull_request/<int:pr_id>/comments', views.get_pr_comments, name='all_comments_on_pr'),
     path('issue/<int:issue_id>/comments', views.get_issue_comments, name='all_comments_on_issue'),
-    path('comments/<int:id>', views.delete_comment_by_id, name='delete_comment'),
-    path('comments/<int:id>', views.edit_comment_by_id, name='edit_comment'),
-    path('comments/add', views.add_new_comment, name='add_comment'),
+    path('comments/<int:id>', views.delete_or_edit_comment, name='delete_or_edit_comment'),
+    path('new-comment', views.add_new_comment, name='add_comment'),
 ]
--- uks_project/user_actions_management_app/views.py
+++ uks_project/user_actions_management_app/views.py
@@ -13,21 +13,21 @@ from rest_framework import status
 @api_view(['GET'])
 def get_all_comments(request):
     comments= Comment.objects.all()
-    if(len(comments) == 0): raise Http404('No Comments matches the given query.')
+    if(len(comments) == 0): raise Http404('No comments found that matches the given query.')
     serializers=CommentSerializer(comments,many=True)
     return Response(serializers.data)
 
 @api_view(['GET'])
 def get_pr_comments(request,pr_id):
-    comments= Comment.objects.filter(issue=pr_id)
-    if(len(comments) == 0): raise Http404('No Comments matches the given query.')
+    comments= Comment.objects.filter(pull_request=pr_id)
+    if(len(comments) == 0): raise Http404('No comments found that matches the given query.')
     serializers=CommentSerializer(comments,many=True)
     return Response(serializers.data)
 
 @api_view(['GET'])
 def get_issue_comments(request,issue_id):
     comments= Comment.objects.filter(issue=issue_id)
-    if(len(comments) == 0): raise Http404('No Comments matches the given query.')
+    if(len(comments) == 0): raise Http404('No comments found that matches the given query.')
     serializers=CommentSerializer(comments,many=True)
     return Response(serializers.data)
 
@@ -40,24 +40,28 @@ def add_new_comment(request,id=None):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['DELETE'])
-def delete_comment_by_id(request,id):
-    try:
-        reaction = Comment.objects.get(comment=id)
-        reaction.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    except Comment.DoesNotExist:
-        reaction = None
-        return Response(status=status.HTTP_404_NOT_FOUND)
+@api_view(['DELETE', 'PUT'])
+def delete_or_edit_comment(request,id):
+    if request.method == 'DELETE':
+        try:
+            comment = Comment.objects.get(id=id)
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Comment.DoesNotExist:
+            comment = None
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    elif request.method == 'PUT':
+        try:
+            comment = Comment.objects.get(id=id)
+        except Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = CommentSerializer(comment, data=request.data)
 
-@api_view(['PUT'])
-def edit_comment_by_id(request,id):
-    try:
-        snippet = Comment.objects.get(pk=id)
-    except Comment.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = Comment(snippet, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
