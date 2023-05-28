c824b5e705e9b64bf60eebdbf364fa8df0186f0f
jovicpetra <jovic.petra99@gmail.com>
2023-05-28 20:01:49 +0200

    feat: api for reactions created

--- uks_project/user_actions_management_app/serializers.py
+++ uks_project/user_actions_management_app/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Comment
+from .models import Comment, Reaction
 
 # File to define Serializers for user_actions_management_app
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = [ "pk", "author", "created_at", "updated_at", "content" ,"issue", "pull_request"]
+        fields = [ "id", "author", "created_at", "updated_at", "content" ,"issue", "pull_request"]
+        read_only_field = ['id']
+
+class ReactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reaction
+        fields = [ "id", "author", "comment", "type"]
+        read_only_field = ['id']
--- uks_project/user_actions_management_app/urls.py
+++ uks_project/user_actions_management_app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('issue/<int:issue_id>/comments', views.get_issue_comments, name='all_comments_on_issue'),
     path('comments/<int:id>', views.delete_or_edit_comment, name='delete_or_edit_comment'),
     path('new-comment', views.add_new_comment, name='add_comment'),
+    path('reactions/', views.get_all_reactions, name='all_reactions'),
+    path('comment/<int:comment_id>/reactions', views.get_comment_reactions, name='all_reactions_on_comment'),
+    path('reactions/<int:id>', views.remove_reaction, name='remove_reaction_on_comment'),
+    path('new-reaction', views.add_new_reaction, name='add_reaction')
 ]
