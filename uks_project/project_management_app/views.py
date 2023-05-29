2f69593c5b8e9b676cbdf5eb1d177319b53866e7
jovana.arsovic@levi9.com <jovana.arsovic@levi9.com>
2023-05-29 20:58:07 +0200

    fix: add viewas.py file

--- uks_project/project_management_app/views.py
+++ uks_project/project_management_app/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from .models import Milestone, Label, Issue
+from .models import Milestone, Label, Issue, PullRequest
 from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from .serializers import IssueSerializer, LabelSerializer, MilestoneSerializer
+from .serializers import IssueSerializer, LabelSerializer, MilestoneSerializer, PullRequestSerializer
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 #Milestone Handling
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_milestone(request,id):
@@ -166,4 +165,58 @@ def delete_issue(request,id):
     except Issue.DoesNotExist:
         issue = None
         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+#PullRequest Handling
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_pull_request(request,id):
+    try:
+        pull_request= PullRequest.objects.get(id=id)
+        serializers = PullRequestSerializer(pull_request)
+        return Response(serializers.data)
+    except PullRequest.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+        
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_all_pull_request(request,id=None):
+    pull_requests = PullRequest.objects.all()
+    if(len(pull_requests) == 0): raise Http404('No pull requests found that matches the given query.')
+    serializers = PullRequestSerializer(pull_requests,many=True)
+    return Response(serializers.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_new_pull_request(request,id=None):
+    serializer = PullRequestSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_pull_request(request,id):
+    try:
+        pull_request= PullRequest.objects.get(id=id)
+    except PullRequest.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = PullRequestSerializer(pull_request, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_pull_request(request,id):
+    try:
+        pull_request = PullRequest.objects.get(id=id)
+        pull_request.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except PullRequest.DoesNotExist:
+        pull_request = None
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
 
