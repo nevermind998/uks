b76db701d223fcb9a1b365a8fad35a515f313b5a
ivanagavric <gavric.ra160.2017@uns.ac.rs>
2023-05-29 21:37:39 +0200

    feat: repository operations handling added

--- uks_project/versioning_management_app/serializers.py
+++ uks_project/versioning_management_app/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
 
-# File to define Serializers for versioning_management_app
+from .models import Repository
+
+
+class RepositorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Repository
+        fields = ['pk', 'name', 'owner', 'description', 'created_at', 'visibility', 'default_branch', 'collaborators']
+        read_only_field = ['pk']
