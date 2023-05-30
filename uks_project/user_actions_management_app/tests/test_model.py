245dfbc2719f2338fc12a5a377360bd98954b4cb
jovicpetra <jovic.petra99@gmail.com>
2023-05-30 20:22:56 +0200

    feat: unit tests for reactions added

--- uks_project/user_actions_management_app/tests/test_model.py
+++ uks_project/user_actions_management_app/tests/test_model.py
@@ -2,7 +2,7 @@ from django.test import Client, TestCase
 from django.urls import reverse
 
 from ..management.set_up_database import Command
-from ..models import Comment, User, Issue
+from ..models import Comment, User, Issue, Reaction
 
 #Unit Tests
 
@@ -24,4 +24,18 @@ class CommentsTests(TestCase):
     def test_successful_get_comments(self):
         comments = Comment.objects.all()
         self.assertEqual(len(comments), 1)  
-        self.assertEqual(comments[0].content, "Jane please finish this issue by this Friday. Thanks.")  
+        self.assertEqual(comments[0].content, "Jane please finish this issue by this Friday. Thanks.")
+
+    def test_add_new_reaction(self):
+        author = User.objects.filter(username="janedoe").first()
+        comment = Comment.objects.filter(content="Jane please finish this issue by this Friday. Thanks.").first()
+        new_reaction = Reaction.objects.create(author=author, comment=comment,  type="LOVE")
+        new_reaction.save()
+        self.assertEqual(new_reaction.type,"LOVE")
+        self.assertEqual(new_reaction.author.given_name,"Jane")
+        self.assertEqual(new_reaction.comment.id, comment.id)
+
+    def test_successful_get_reactions(self):
+        reactions = Reaction.objects.all()
+        self.assertEqual(len(reactions), 1)  
+        self.assertEqual(reactions[0].type, "LIKE")  
