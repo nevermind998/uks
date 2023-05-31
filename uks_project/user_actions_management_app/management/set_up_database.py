3b75cabe191d1703f11692ce0a7c5735c6b319ac
jovicpetra <jovic.petra99@gmail.com>
2023-05-31 20:25:14 +0200

    feat: tests for repo actions api added

--- uks_project/user_actions_management_app/management/set_up_database.py
+++ uks_project/user_actions_management_app/management/set_up_database.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from ..models import User, Repository, Issue, PullRequest, Comment, Reaction
+from ..models import User, Repository, Issue, PullRequest, Comment, Reaction, Action
 
 
 class Command(BaseCommand):
@@ -10,6 +10,7 @@ class Command(BaseCommand):
         Issue.objects.all().delete()
         Comment.objects.all().delete()
         Reaction.objects.all().delete()
+        Action.objects.all().delete()
 
         user1 = User.objects.create_user(
             given_name="John",
@@ -27,12 +28,20 @@ class Command(BaseCommand):
         )
         repo = Repository(name="Project Repository", owner=user1, description="This is our repository.", visibility="PUBLIC", default_branch="main")
         repo.save()
+        repo2 = Repository(name="Forked Repository", owner=user1, description="This is our repository.", visibility="PUBLIC", default_branch="main")
+        repo2.save()
         issue = Issue(title="First Issue", status="OPEN", repository=repo, author=user1)
         issue.save()
         comment = Comment(author=user1, content="Jane please finish this issue by this Friday. Thanks.",  issue=issue)
         comment.save()
         reaction = Reaction(author=user2, comment=comment, type="LIKE")
         reaction.save()
+        action1 = Action(author=user2, type="STAR", repository=repo)
+        action1.save()
+        action2 = Action(author=user1, type="WATCH", repository=repo)
+        action2.save()
+        action3 = Action(author=user2, type="FORK", repository=repo, forked_repo=repo2)
+        action3.save()
 
 
     def handle(self, *args, **options):
---
+++ uks_project/user_actions_management_app/tests/test_repo_actions.py
@@ -0,0 +1,146 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from ..management.set_up_database import Command
+import json
+from ..models import User, Action, Repository
+
+#Integration Tests
+
+JSON = 'application/json'
+
+def get_jwt_token():
+    client = Client()
+    credentials = {"email": "johndoe@test.com", "password": "123"}
+    response = client.post(reverse("sign-in"), credentials)
+    y = json.loads(response.content)
+    return  y.get("access_token", None)
+
+class ActionsApiTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        c = Command()
+        c.handle()
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.token = f'Bearer {get_jwt_token()}'
+
+    def test_get_user_stars(self):
+        user = User.objects.get(username="janedoe").id
+        response = self.client.get('/user-actions/user-profile/'+str(user)+'/stars', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(res_obj),1)
+
+    def test_get_all_actions_wrong_path(self):
+        response = self.client.get('/user-actions/actions', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_user_stars_unauth(self):
+        user = User.objects.get(username="janedoe").id
+        response = self.client.get('/user-actions/user-profile/'+str(user)+'/stars', HTTP_AUTHORIZATION="", content_type=JSON)
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_action_for_user(self):
+        user = User.objects.get(username="janedoe").id
+        repository = Repository.objects.get(name="Project Repository").id
+        response = self.client.get('/user-actions/repository/'+str(repository)+'/user/'+str(user)+'/star', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res_obj['type'], 'STAR')
+    
+    def test_get_action_for_user_fail(self):
+        user = User.objects.get(username="johndoe").id
+        repository = Repository.objects.get(name="Project Repository").id
+        response = self.client.get('/user-actions/repository/'+str(repository)+'/user/'+str(user)+'/star', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_repo_stargazers(self):
+        repository = Repository.objects.get(name="Project Repository").id
+        response = self.client.get('/user-actions/repository/'+str(repository)+'/stargazers', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(len(res_obj),1)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_repo_stargazers_fail(self):
+        response = self.client.get('/user-actions/repository/89/stargazers', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_repo_watchers(self):
+        repository = Repository.objects.get(name="Project Repository").id
+        response = self.client.get('/user-actions/repository/'+str(repository)+'/watchers', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(len(res_obj),1)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_repo_watchers_fail(self):
+        response = self.client.get('/user-actions/repository/89/watchers', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_repo_forks(self):
+        repository = Repository.objects.get(name="Project Repository").id
+        response = self.client.get('/user-actions/repository/'+str(repository)+'/forked-repos', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(len(res_obj),1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res_obj[0]['name'], 'Forked Repository')
+
+    
+    def test_get_repo_forks_fail(self):
+        response = self.client.get('/user-actions/repository/89/forked-repos', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+
+    def test_post_create_action(self):
+        author = User.objects.get(username="johndoe").id
+        repository = Repository.objects.get(name="Project Repository").id
+        comment = {
+        "type": "WATCH",
+        "author": author,
+        "repository": repository
+        }
+
+        response = self.client.post(
+            '/user-actions/new-action',
+            data=json.dumps(comment),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )
+        res_obj = json.loads(response.content)
+       
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(res_obj['type'], 'WATCH')
+    
+    def test_post_create_comment_fail(self):
+        author = User.objects.get(username="johndoe").id
+        repository = Repository.objects.get(name="Project Repository").id
+        comment = {
+        "type": "MOON",
+        "author": author,
+        "repository": repository
+        }
+
+        response = self.client.post(
+            '/user-actions/new-action',
+            data=json.dumps(comment),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )
+        self.assertEqual(response.status_code, 400)
+    
+    def test_delete_action(self):
+        action = Action.objects.get(type="STAR").id
+        response = self.client.delete(
+            '/user-actions/actions/'+str(action),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )       
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_action_fail(self):
+        response = self.client.delete(
+            '/user-actions/actions/99',
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )       
+        self.assertEqual(response.status_code, 404)
