a6ffacb3f7adcce0a6d6e92d9b4edc1753c3ba67
jovana.arsovic@levi9.com <jovana.arsovic@levi9.com>
2023-05-31 15:23:51 +0200

    feat: integration and unit tests for issue added

--- uks_project/project_management_app/tests/test_user_action.py
+++ uks_project/project_management_app/tests/test_user_action.py
@@ -248,3 +248,175 @@ class LabelApiTests(TestCase):
             content_type=JSON
         )       
         self.assertEqual(response.status_code, 404)
+
+class IssueApiTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        c = Command()
+        c.handle()
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.token = f'Bearer {get_jwt_token()}'
+
+    def test_get_all_issue(self):
+        response = self.client.get('/project/issues/', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(len(res_obj),1)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_all_issue_wrong_path(self):
+        response = self.client.get('/project/issue/all', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_all_issue_unauth(self):
+        response = self.client.get('/project/issues/', HTTP_AUTHORIZATION="", content_type=JSON)
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_issue_by_status(self):
+        response = self.client.get('/project/issues/OPEN/status', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertGreaterEqual(len(res_obj),1)
+
+    def test_get_issue_by_status_fail(self):
+        response = self.client.get('/project/issues/APPROVED/status', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_issue_by_author(self):
+        author = User.objects.get(given_name='John')
+        response = self.client.get('/project/issues/'+str(author.id)+'/author', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertGreaterEqual(len(res_obj),1)
+
+    def test_get_issue_by_author_fail(self):
+        response = self.client.get('/project/issues/55/author', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_issue_by_label(self):
+        label = Label.objects.get(name='Label')
+        response = self.client.get('/project/issues/'+str(label.id)+'/label', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertGreaterEqual(len(res_obj),1)
+
+    def test_get_issue_by_label_fail(self):
+        response = self.client.get('/project/issues/55/label', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_issue_by_milestone(self):
+        milestone = Milestone.objects.get(title='Milestone')
+        response = self.client.get('/project/issues/'+str(milestone.id)+'/milestone', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertGreaterEqual(len(res_obj),1)
+
+    def test_get_issue_by_milestone_fail(self):
+        response = self.client.get('/project/issues/55/milestone', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_issue_by_assignee(self):
+        assignee = User.objects.get(given_name='Jane')
+        response = self.client.get('/project/issues/'+str(assignee.id)+'/assignee', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertGreaterEqual(len(res_obj),1)
+
+    def test_get_issue_by_assignee_fail(self):
+        response = self.client.get('/project/issues/55/assignee', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        self.assertEqual(response.status_code, 404)
+
+    def test_post_create_issue(self):
+        repository = Repository.objects.get(name='Project Repository').id
+        assignee = User.objects.get(given_name='Jane').id
+        milestone = Milestone.objects.get(title='Milestone').id
+        author = User.objects.get(given_name='John').id
+        label = Label.objects.get(name='Label').id
+        request = {
+            "repository": repository,
+            "title": "New issue",
+            "created_at": "2022-03-12",
+            "status": "OPEN",
+            "milestone": milestone,
+            "labels":[label],
+            "author": author,
+            "assignees": [assignee]
+        }
+
+        response = self.client.post(
+            '/project/new-issue',
+            data=json.dumps(request),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )
+
+        res_obj = json.loads(response.content)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(res_obj['title'], 'New issue')
+
+    def test_post_create_issue_fail(self):
+        request = {
+            "name": "Fail issue",
+            }
+
+        response = self.client.post(
+            '/project/new-issue',
+            data=json.dumps(request),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )       
+        self.assertEqual(response.status_code, 400)
+
+    def test_update_issue(self):
+        issue = Issue.objects.get(title="First Issue")
+        repository = Repository.objects.get(name='Project Repository').id
+        author = User.objects.get(given_name='John').id
+        request = {
+            "repository": repository,
+            "title": "Update issue",
+            "status": "OPEN",
+            "author": author
+            }
+        response = self.client.put(
+            '/project/update-issue/'+str(issue.id),
+            data=json.dumps(request),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )       
+        res_obj = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res_obj['title'], 'Update issue')
+
+    def test_upate_issue_fail(self):
+        issue = Issue.objects.get(title="First Issue")
+        repository = Repository.objects.get(name='Project Repository').id
+        request = {
+            "repository": repository,
+            "title": "Update issue",
+        }
+        response = self.client.put(
+            '/project/update-issue/'+str(issue.id),
+            data=json.dumps(request),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )       
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_issue(self):
+        issue = Issue.objects.get(title="First Issue")
+        response = self.client.delete(
+            '/project/delete-issue/'+str(issue.id),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )       
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_issue_fail(self):
+        response = self.client.delete(
+            '/project/delete-issue/98',
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )       
+        self.assertEqual(response.status_code, 404)
