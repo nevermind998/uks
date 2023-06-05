4178b53af86cd9c1c90ca4f6a46f1f6f112afde4
jovana.arsovic@levi9.com <jovana.arsovic@levi9.com>
2023-06-05 09:00:22 +0200

    fix: removing optional value from scheme check

--- uks_front/src/Views/ProjectManagement/MilestoneForm/milestoneSchema.ts
+++ uks_front/src/Views/ProjectManagement/MilestoneForm/milestoneSchema.ts
@@ -1,7 +1,5 @@
 import * as Yup from 'yup';
 
 export const MILESTONE_SCHEMA = Yup.object().shape({
-  title:Yup.string().required('You need to provide title!'),
-  due_date: Yup.string().required('You need to provide date!'),
-  description:Yup.string().required('You need to provide description!'),
+  title:Yup.string().required('You need to provide title!')
 });
